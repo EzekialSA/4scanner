414fc294715270ab2a708cce3442775d633d9a75
Lacsap- <lacsap@cock.li>
2016-04-07 08:42:46 -0400

    Changed version to 1.0.6

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='4scanner',
-    version='1.0.5',
+    version='1.0.6',
     description='4chan threads scanner',
     long_description=long_description,
     url='https://github.com/Lacsap-/4scanner',
