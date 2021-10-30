8491fb639fe36a047acc76c0154269d310c89e55
Tom Hesse <heyhoman@mailbox.org>
2021-10-30 10:41:45 +0200

    Use xdg base directory specification for db

--- scanner/config.py
+++ scanner/config.py
@@ -1,7 +1,14 @@
 # Used to store package wide constants
 import os
 
-DB_FILE = os.path.expanduser("~/.4scanner/4scanner.db")
+if os.path.isdir(os.path.expanduser("~/.4scanner")):
+    DB_FILE = os.path.expanduser("~/.4scanner/4scanner.db")
+elif os.getenv("XDG_DATA_HOME"):
+    DB_FILE = os.path.join(os.getenv("XDG_DATA_HOME"), "4scanner", "4scanner.db")
+elif os.getenv("APPDATA"):
+    DB_FILE = os.path.join(os.getenv("APPDATA"), "4scanner", "4scanner.db")
+else:
+    DB_FILE = os.path.join(os.getenv("HOME"), ".local", "share", "4scanner", "4scanner.db")
 
 # Global variable used to keep track of what is downloading
 currently_downloading = []
